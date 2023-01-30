dc4cb2570a4c7c43f7b607e7d51fd4a0cff4af93
Rémy <remyc59440@hotmail.com>
2023-01-30 10:17:33 +0100

    kk

--- classes/Princesse.php
+++ classes/Princesse.php
@@ -5,4 +5,19 @@ class Princesse extends Personnage
     private bool $saved;
     public $x = 450;
     public $y = 450;
+
+    /**
+     * @param bool $saved
+     * @param int $x
+     * @param int $y
+     */
+    public function __construct(bool $saved, int $x, int $y)
+    {
+        $this->saved = $saved;
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+
+
 }
